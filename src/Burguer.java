92c50d2a4a22de4b4b82a33cea3464fe31fdfcae
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2026-02-16 13:45:33 +0000

    Basic setup

---
+++ src/Burguer.java
@@ -0,0 +1,7 @@
+public class Burguer {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("I'm a burguer!");
+        }
+    }
+}
